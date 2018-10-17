552f1a5841db850fa6ea60e18f08dece7056d8df
1618641 <1618641@qq.com>
2018-10-17 15:43:03 +0800

    第一次提交

---
+++ src/org/java/demo/Demo.java
@@ -0,0 +1,12 @@
+package org.java.demo;
+
+/**
+ * @Author: 马果
+ * @Date: 2018/10/17 15:37
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello..............");
+    }
+}
